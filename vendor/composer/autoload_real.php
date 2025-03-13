1c3611b0562c138462a8aab113d131630b04ec29
damiosdam <m.arsyad.ikbar.14@gmail.com>
2025-03-14 01:50:32 +0700

    aa

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit58024d93bcd7cb560cea7daaab0aee79
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit58024d93bcd7cb560cea7daaab0aee79::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
